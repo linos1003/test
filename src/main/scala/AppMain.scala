fcc77fece91d0d353e85b88a78c819b42d1c337b
SMIDA Bassem <ext.bassem.smida@sncf.fr>
2017-11-20 11:43:40 +0100

    active logging

---
+++ src/main/scala/AppMain.scala
@@ -0,0 +1,17 @@
+import org.apache.log4j.{Logger, BasicConfigurator}
+
+/**
+  * Created by bsmida on 19/11/17.
+  */
+object AppMain extends App{
+
+  val LOGGER= Logger.getLogger(this.getClass)
+
+  if(args.length<2)
+    LOGGER.error(
+      """
+        |Wrong number of parameters
+        |Usage: AppMain <input-file-path> <output-file-path>
+      """.stripMargin
+    )
+}
--- src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-import models.Orientation._
-
-/**
-  * Created by bsmida on 19/11/17.
-  */
-object Main extends App{
-
-
-}
